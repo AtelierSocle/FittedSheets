0d2655c77f380776de38025e5cba5e4cd018d397
Sweefties <wladdicario@icloud.com>
2018-11-28 00:15:16 +0100

    Added Carthage support

---
+++ FittedSheets-iOS/FittedSheets_iOS.h
@@ -0,0 +1,19 @@
+//
+//  FittedSheets_iOS.h
+//  FittedSheets-iOS
+//
+//  Created by Wlad Dicario on 28/11/2018.
+//  Copyright © 2018 Gordon Tucker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FittedSheets_iOS.
+FOUNDATION_EXPORT double FittedSheets_iOSVersionNumber;
+
+//! Project version string for FittedSheets_iOS.
+FOUNDATION_EXPORT const unsigned char FittedSheets_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FittedSheets_iOS/PublicHeader.h>
+
+
